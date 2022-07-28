27b9a07549346fc0cfff49adf88a42073895dcf3
chris <chrisabbod@gmail.com>
2022-07-28 12:29:16 -0400

    Clicking button gives random fact from provided set of facts

--- app/src/main/java/com/chrisabbod/catfacts/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/catfacts/MainActivity.kt
@@ -2,10 +2,34 @@ package com.chrisabbod.catfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chrisabbod.catfacts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val catFactList: Set<String> = setOf(
+        "Cats are animals",
+        "Cats are usually small",
+        "Cats are usually furry",
+        "Cats are cute!",
+        "Cats are troublemakers",
+        "Lions are cats",
+        "Tigers are cats",
+        "Jaguars are cats",
+        "Panthers are cats",
+        "Cheetahs are cats"
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnRandomFact.setOnClickListener {
+                tvRandomFact.text = catFactList.random()
+            }
+        }
     }
 }
