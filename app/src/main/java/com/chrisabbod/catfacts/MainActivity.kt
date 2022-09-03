254efab46bf9cb026e05876b92ad7672db89e711
Christopher <chrisabbod@gmail.com>
2022-09-03 15:46:03 -0400

    Refactor coroutine for getting single cat fact

--- app/src/main/java/com/chrisabbod/catfacts/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/catfacts/MainActivity.kt
@@ -4,28 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.chrisabbod.catfacts.databinding.ActivityMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val catFactsAPI: CatFactsAPI = RetrofitHelper.getInstance().create(CatFactsAPI::class.java)
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + parentJob)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val catFactsAPI = RetrofitHelper.getInstance().create(CatFactsAPI::class.java)
-
-        GlobalScope.launch {
+        coroutineScope.launch(Dispatchers.Main) {
             val catFactList = catFactsAPI.retrieveListOfFacts(300, 100)
 
-            binding.apply {
-                btnRandomFact.setOnClickListener {
-                    tvRandomFact.text = catFactList.data.random().fact
-                }
-            }
+            binding.btnRandomFact.setOnClickListener { getCatFact(catFactList) }
         }
+    }
+
+    private fun getCatFact(listOfFacts: CatFactResponse) {
+        binding.tvRandomFact.text = listOfFacts.data.random().fact
     }
 }
